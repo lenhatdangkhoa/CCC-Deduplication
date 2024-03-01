27ca02d3941c7264615cb8a7c8a880c1213173ee
Khoa Le <kl58277@uga.edu>
2024-03-01 16:38:36 -0500

    finished all level of duplication

--- script.py
+++ script.py
@@ -2,16 +2,22 @@ import pandas as pd
 import numpy as np
 import re # Regex Library
 from difflib import SequenceMatcher # Sequence Scoring
+import os,sys
 
 
 class Deduplication:
+
     data = pd.DataFrame()
     processed_data = pd.DataFrame()
 
-    def __init__(self):
-        self.data = pd.read_csv("data/test.csv")
+    """
+    Initialize Deduplication class that automatically normalize data and find duplicates.
+    """
+    def __init__(self, path):
+        self.data = pd.read_csv(path)
         self.processed_data = self.normalize(self.data)
         self.level1dup(self.processed_data)
+        self.level2dup(self.processed_data)
     """
     Clean and apply data normalization.
     Params:
@@ -49,7 +55,10 @@ class Deduplication:
     """
     def level1dup(self,test_data : pd.DataFrame) -> None:
         new_data = pd.DataFrame(columns=self.data.columns.to_list())
+        new_data.insert(0, "DUPLICATE_ID",value=None, allow_duplicates=True) # Add new duplicate id for grouping
+        dup_index = 0
         for index, row in test_data.iterrows():
+            increment = False
             for index2, row2 in test_data.iloc[index+1:].iterrows():
                 scores=[]
                 scores.append(SequenceMatcher(None, row["FIRSTNAME"],row2["FIRSTNAME"]).ratio())
@@ -64,10 +73,93 @@ class Deduplication:
                     scores.append(SequenceMatcher(None, str(row["MOBILEPHONE"]),str(row2["MOBILEPHONE"])).ratio())
                 if (self.harmonic_mean(scores) >= 0.9):
                     new_data = pd.concat([new_data,row.to_frame().T], ignore_index=True)
+                    new_data.loc[new_data.shape[0]-1,"DUPLICATE_ID"] = dup_index
                     new_data = pd.concat([new_data,row2.to_frame().T], ignore_index=True)
+                    new_data.loc[new_data.shape[0]-1,"DUPLICATE_ID"] = dup_index
                     test_data = test_data.drop(index=index2)
-                    print(self.harmonic_mean(scores))
-        print(new_data)
+                    increment = True
+            if increment:
+                dup_index += 1
+                increment = False
         new_data.to_csv("duplicates/level1duplicate.csv", index=False)
 
-dedupl = Deduplication()
+    """
+    Find the second level of duplication. This is where the duplication is somewhat visible (75% similarity).
+    Args:
+        test_data (pd.DataFrame): the data to find duplication
+    Returns:
+        None
+    """
+    def level2dup(self,test_data : pd.DataFrame) -> None:
+        new_data = pd.DataFrame(columns=self.data.columns.to_list())
+        new_data.insert(0, "DUPLICATE_ID",value=None, allow_duplicates=True) # Add new duplicate id for grouping
+        dup_index = 0
+        for index, row in test_data.iterrows():
+            increment = False
+            for index2, row2 in test_data.iloc[index+1:].iterrows():
+                scores=[]
+                scores.append(SequenceMatcher(None, row["FIRSTNAME"],row2["FIRSTNAME"]).ratio())
+                scores.append(SequenceMatcher(None, row["LASTNAME"],row2["LASTNAME"]).ratio())
+                scores.append(SequenceMatcher(None, row["EMAIL"], row2["EMAIL"]).ratio())
+                scores.append(SequenceMatcher(None, str(row["OTHERSTREET"]),str(row2["OTHERSTREET"])).ratio())
+                if str(row["PHONE"]) != "0000000000" and str(row2["PHONE"]) != "0000000000":
+                    scores.append(SequenceMatcher(None, str(row["PHONE"]),str(row2["PHONE"])).ratio())
+                if str(row["OTHERPHONE"]) != "0000000000" and str(row2["OTHERPHONE"]) != "0000000000":
+                    scores.append(SequenceMatcher(None, str(row["OTHERPHONE"]),str(row2["OTHERPHONE"])).ratio())
+                if str(row["MOBILEPHONE"]) != "0000000000" and str(row2["MOBILEPHONE"]) != "0000000000":
+                    scores.append(SequenceMatcher(None, str(row["MOBILEPHONE"]),str(row2["MOBILEPHONE"])).ratio())
+                if (self.harmonic_mean(scores) >= 0.75):
+                    new_data = pd.concat([new_data,row.to_frame().T], ignore_index=True)
+                    new_data.loc[new_data.shape[0]-1,"DUPLICATE_ID"] = dup_index
+                    new_data = pd.concat([new_data,row2.to_frame().T], ignore_index=True)
+                    new_data.loc[new_data.shape[0]-1,"DUPLICATE_ID"] = dup_index
+                    test_data = test_data.drop(index=index2)
+                    increment = True
+            if increment:
+                dup_index += 1
+                increment = False
+        new_data.to_csv("duplicates/level2duplicate.csv", index=False)
+
+    """
+    Find the third level of duplication. This is where the duplication is 50/50 (50% similarity).
+    Args:
+        test_data (pd.DataFrame): the data to find duplication
+    Returns:
+        None
+    """
+    def level2dup(self,test_data : pd.DataFrame) -> None:
+        new_data = pd.DataFrame(columns=self.data.columns.to_list())
+        new_data.insert(0, "DUPLICATE_ID",value=None, allow_duplicates=True) # Add new duplicate id for grouping
+        dup_index = 0
+        for index, row in test_data.iterrows():
+            increment = False
+            for index2, row2 in test_data.iloc[index+1:].iterrows():
+                scores=[]
+                scores.append(SequenceMatcher(None, row["FIRSTNAME"],row2["FIRSTNAME"]).ratio())
+                scores.append(SequenceMatcher(None, row["LASTNAME"],row2["LASTNAME"]).ratio())
+                scores.append(SequenceMatcher(None, row["EMAIL"], row2["EMAIL"]).ratio())
+                scores.append(SequenceMatcher(None, str(row["OTHERSTREET"]),str(row2["OTHERSTREET"])).ratio())
+                if str(row["PHONE"]) != "0000000000" and str(row2["PHONE"]) != "0000000000":
+                    scores.append(SequenceMatcher(None, str(row["PHONE"]),str(row2["PHONE"])).ratio())
+                if str(row["OTHERPHONE"]) != "0000000000" and str(row2["OTHERPHONE"]) != "0000000000":
+                    scores.append(SequenceMatcher(None, str(row["OTHERPHONE"]),str(row2["OTHERPHONE"])).ratio())
+                if str(row["MOBILEPHONE"]) != "0000000000" and str(row2["MOBILEPHONE"]) != "0000000000":
+                    scores.append(SequenceMatcher(None, str(row["MOBILEPHONE"]),str(row2["MOBILEPHONE"])).ratio())
+                if (self.harmonic_mean(scores) >= 0.5):
+                    new_data = pd.concat([new_data,row.to_frame().T], ignore_index=True)
+                    new_data.loc[new_data.shape[0]-1,"DUPLICATE_ID"] = dup_index
+                    new_data = pd.concat([new_data,row2.to_frame().T], ignore_index=True)
+                    new_data.loc[new_data.shape[0]-1,"DUPLICATE_ID"] = dup_index
+                    test_data = test_data.drop(index=index2)
+                    increment = True
+            if increment:
+                dup_index += 1
+                increment = False
+        new_data.to_csv("duplicates/level3duplicate.csv", index=False)
+
+if __name__ == "__main__":
+    try: 
+        dedupl = Deduplication(os.getcwd() + "/" + sys.argv[1])
+    except IndexError:
+        raise Exception("Missing path to file. Try \"python3 script.py <path/to/file>\"")
+
